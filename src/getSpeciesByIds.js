45cb5076ad8ad3e07ad2ed8f31504668e378672c
Bruno <bruno_bury@hotmail.com>
2023-04-07 21:18:45 -0300

    requisito 1 getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,5 +3,4 @@ const data = require('../data/zoo_data');
 const getSpeciesByIds = (...ids) => (ids.length === 0
   ? [] : data.species.filter((specie) => ids.includes(specie.id)));
 
-console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
