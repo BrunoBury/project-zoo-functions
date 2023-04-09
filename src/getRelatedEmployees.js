c9c7911093c5508acda179fa6189b5d6d32fc2fe
Bruno <bruno_bury@hotmail.com>
2023-04-09 17:17:15 -0300

    requisto 4, getRelatedEmployees retorno true or false, e nome de empregados gerenciado por pessoa

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => data.employees.flatMap((employee) =>
+  employee.managers).some((manager) => manager === id);
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((employee) => employee.managers.includes(managerId))
+    .map((func) => `${func.firstName} ${func.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
