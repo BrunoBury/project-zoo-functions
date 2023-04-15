0883dcaa994f9bf688f59088d1a139f86963b6ba
Bruno <bruno_bury@hotmail.com>
2023-04-15 15:25:22 -0300

    requisito 5 função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,5 +1,16 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
+  if (!animal) {
+    const obj = {};
+    data.species.forEach((specie) => { obj[specie.name] = specie.residents.length; });
+    return obj;
+  }
+  if (Object.keys(animal).length === 1) {
+    return data.species.find((specie) => specie.name === animal.species).residents.length;
+  }
+  return data.species.find((specie) => specie.name === animal.species)
+    .residents.filter((resident) => resident.sex === animal.sex).length;
 };
+
 module.exports = countAnimals;
