27a99ad944cf13105e6919b34ec2b0bc140aed35
Bruno <bruno_bury@hotmail.com>
2023-04-15 16:42:24 -0300

    requisito 6 cobertura de testes

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,28 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  it('Verifica retorno undefined', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  it('Verifica param !== string', () => {
+    expect(handlerElephants(123)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verifica parametro = name', () => {
+    expect(handlerElephants('name')).toBe('elephants');
+  });
+  it('Verifica quantidade residents', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verifica nomes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Verifica  media de idade', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Verifica retorno null', () => {
+    expect(handlerElephants('age')).toBe(null);
+  });
+});
